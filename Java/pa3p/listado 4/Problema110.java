68b02aae1f1feecbea7c291e71c00a8bb82df0b9
Falling-Bridge <lucmorales2023@inf.udec.cl>
2024-04-22 21:24:53 -0400

    i deleted some spaces

--- Java/pa3p/listado 4/Problema110.java
+++ Java/pa3p/listado 4/Problema110.java
@@ -107,6 +107,8 @@ class Deposito {
     }
 }
 
+class DepositoD{}
+
 class DepositoM {
 
     private ArrayList<Moneda> lista2;
@@ -234,6 +236,18 @@ class CocaCola extends Bebida {
 
 public class Problema110 {
     public static void main(String[] args) {
+        Expendedor exp = new Expendedor(1,1000);
+        Moneda m = null;
+        Comprador c=null;
 
+        m = new Moneda1500();
+        c = new Comprador(m,1,exp);
+
+        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
+
+        m = new Moneda1500();
+        c = new Comprador(m,1,exp);
+
+        System.out.println(c.queBebiste() + ", " + c.cuantoVuelto());
     }
 }
